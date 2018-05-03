8078ad6953e1c0c65096fff0cd9d6d36935df404
Marc Arndt <marc@marcarndt.com>
2018-05-03 11:19:09 +0200

    adding dashboard

---
+++ src/components/dashboard.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-container fluid>
+      <country v-for="country in data" v-bind:key="country" v-bind:country="Object.entries(country)[0][0]" v-bind:files="Object.entries(country)[0][1]"/>
+  </b-container>
+</template>
+
+<script>
+import Country from './country'
+export default {
+  name: 'dashboard',
+  components: {Country},
+  data () {
+    return {
+      data: ''
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      this.$http.get(process.env.ENDPOINT)
+        .then(response => {
+          return response.json()
+        }).then(response => {
+          this.data = response
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/file.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="file, col-1">
+    <div class="circle" v-bind:style="styleObject">{{filename }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'file',
+  props: ['ok', 'filename'],
+  data () {
+    return {
+      styleObject: {
+        'background-color': this.ok ? 'green' : 'red'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .circle {
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+    background-color: green
+  }
+
+  .file {
+    text-align: center;
+    vertical-align: middle;
+    line-height: 90px;
+    margin: 5px;
+  }
+</style>
