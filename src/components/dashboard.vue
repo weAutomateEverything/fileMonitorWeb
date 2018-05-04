aaf20543c85479dbb2899e02345a99d0dcb6adcc
Marc Arndt <marc@marcarndt.com>
2018-05-04 08:49:37 +0200

    matrix

--- src/components/dashboard.vue
+++ src/components/dashboard.vue
@@ -1,17 +1,24 @@
 <template>
   <b-container fluid>
-      <country v-for="country in data" v-bind:key="country" v-bind:country="Object.entries(country)[0][0]" v-bind:files="Object.entries(country)[0][1]"/>
+      <div class="row">
+        <div class="col-3">&nbsp;</div>
+        <div class="col-1" v-for="title in countries" v-bind:key="title">{{ title }}</div>
+      </div>
+      <div class="row" v-for="file in files" v-bind:key="file">
+        <div class="col-3">{{ file }}</div>
+        <div class="col-1"  v-for="title in countries" v-bind:key="file+title">{{ getFile(title,file)}}</div>
+      </div>
   </b-container>
 </template>
 
 <script>
-import Country from './country'
 export default {
   name: 'dashboard',
-  components: {Country},
   data () {
     return {
-      data: ''
+      data: new Map(),
+      countries: '',
+      files: ''
     }
   },
   mounted () {
@@ -23,8 +30,36 @@ export default {
         .then(response => {
           return response.json()
         }).then(response => {
-          this.data = response
+          this.data = new Map()
+          this.countries = []
+          this.files = []
+          var rows = Object.entries(response)
+          for (var row in rows) {
+            var rowdata = new Map()
+            var parent = rows[row]
+            var c = Object.entries(parent[1])
+            var country = c[0][0]
+            this.countries.push(country)
+            for (var filename in c[0][1]) {
+              if (!this.files.includes(filename)) {
+                this.files.push(filename)
+              }
+              rowdata.set(filename, c[0][1][filename])
+            }
+            this.data.set(country, rowdata)
+          }
         })
+    },
+    getFile: function (title, file) {
+      if (this.data === undefined) {
+        return ''
+      }
+      var country = this.data.get(title)
+      var fileStatus = country.get(file)
+      if (fileStatus === undefined) {
+        return 'N/A'
+      }
+      return fileStatus
     }
   }
 }
