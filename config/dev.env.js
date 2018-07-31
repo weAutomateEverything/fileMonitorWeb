d227928df4dd8f4388d4015ea236b440cf962de8
Jurgen Schatz <jurgen.schatz@standardbank.co.za>
2018-07-31 17:37:48 +0200

    add-tanzania

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,6 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   ENDPOINT: '"http://127.0.0.1:8002/fileStatus"',
+  // ENDPOINT: '"http://armonitordev.cloudy.standardbank.co.za:8002/fileStatus"',
   BACKDATED: '"http://127.0.0.1:8002/backdated?date="'
   // BACKDATED: '"http://armonitor.cloudy.standardbank.co.za:8002/backdated?date="'
 })
