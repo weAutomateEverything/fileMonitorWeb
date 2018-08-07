0624e4619545b2785035a44e64e5d0867d9e504f
Jurgen Schatz <jurgen.schatz@standardbank.co.za>
2018-08-07 07:38:18 +0200

    temp fix reversal

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,8 +1,8 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // ENDPOINT: '"http://armonitor.cloudy.standardbank.co.za:8002/fileStatus"',
-  // BACKDATED: '"http://armonitor.cloudy.standardbank.co.za:8002/backdated?date="'
-  ENDPOINT: '"http://armonitordev.cloudy.standardbank.co.za:8002/fileStatus"',
-  BACKDATED: '"http://armonitordev.cloudy.standardbank.co.za:8002/backdated?date="'
+  ENDPOINT: '"http://armonitor.cloudy.standardbank.co.za:8002/fileStatus"',
+  BACKDATED: '"http://armonitor.cloudy.standardbank.co.za:8002/backdated?date="'
+  // ENDPOINT: '"http://armonitordev.cloudy.standardbank.co.za:8002/fileStatus"',
+  // BACKDATED: '"http://armonitordev.cloudy.standardbank.co.za:8002/backdated?date="'
 }
